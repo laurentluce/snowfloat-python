ebbc7bf265ece03210ccb2b3ad8a9753659eb8af
Laurent Luce <laurentluce49@yahoo.com>
2013-04-22 13:26:59 -0400

    Add real example test

--- tests_live.py
+++ tests_live.py
@@ -93,11 +93,11 @@ class Tests(unittest.TestCase):
         print 'add points: %.2f' % (time.time() - t)
         for i in range(10000):
             self.assertAlmostEqual(points[i].coordinates[0],
-                pts[i].coordinates[0], 4)
+                pts[i].coordinates[0], 10)
             self.assertAlmostEqual(points[i].coordinates[1],
-                pts[i].coordinates[1], 4)
+                pts[i].coordinates[1], 10)
             self.assertAlmostEqual(points[i].coordinates[2],
-                pts[i].coordinates[2], 3)
+                pts[i].coordinates[2], 10)
             self.assertEqual(points[i].type, 'Point')
             self.assertAlmostEqual(points[i].ts, pts[i].ts, 2)
             self.assertEqual(points[i].dat, pts[i].dat)
@@ -110,11 +110,11 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(points), 10000)
         for i in range(10000):
             self.assertAlmostEqual(points[i].coordinates[0],
-                pts[i].coordinates[0], 4)
+                pts[i].coordinates[0], 10)
             self.assertAlmostEqual(points[i].coordinates[1],
-                pts[i].coordinates[1], 4)
+                pts[i].coordinates[1], 10)
             self.assertAlmostEqual(points[i].coordinates[2],
-                pts[i].coordinates[2], 3)
+                pts[i].coordinates[2], 10)
             self.assertEqual(points[i].type, 'Point')
             self.assertAlmostEqual(points[i].ts, pts[i].ts, 2)
             self.assertEqual(points[i].dat, pts[i].dat)
@@ -215,11 +215,11 @@ class Tests(unittest.TestCase):
         for i in range(10000):
             for j in range(4):
                 self.assertAlmostEqual(polygons[i].coordinates[0][j][0],
-                    pgs[i].coordinates[0][j][0], 4)
+                    pgs[i].coordinates[0][j][0], 10)
                 self.assertAlmostEqual(polygons[i].coordinates[0][j][1],
-                    pgs[i].coordinates[0][j][1], 4)
+                    pgs[i].coordinates[0][j][1], 10)
                 self.assertAlmostEqual(polygons[i].coordinates[0][j][2],
-                    pgs[i].coordinates[0][j][2], 3)
+                    pgs[i].coordinates[0][j][2], 10)
             self.assertEqual(polygons[i].type, 'Polygon')
             self.assertAlmostEqual(polygons[i].ts, pgs[i].ts, 2)
             self.assertEqual(polygons[i].dat, pgs[i].dat)
@@ -316,17 +316,16 @@ class Tests(unittest.TestCase):
                 for j in range(10000)]
             points = self.client.add_geometries(containers[i].id, pts)
 
-        tasks = [
-            {'operation': snowfloat.task.Operations.stats,
-             'resource': 'points',
-             'container_id': containers[0].id,
-             'ts_range': (0, time.time())},
-            {'operation': snowfloat.task.Operations.stats,
-             'resource': 'points',
-             'container_id': containers[0].id,
-             'ts_range': (0, time.time())},
-                ]
-
+        tasks = [snowfloat.task.Task(
+                    operation=snowfloat.task.Operations.stats,
+                    resource='points',
+                    container_id=containers[0].id,
+                    ts_range=(0, time.time())),
+                 snowfloat.task.Task(
+                    operation=snowfloat.task.Operations.stats,
+                    resource='points',
+                    container_id=containers[1].id,
+                    ts_range=(0, time.time()))]
         t = time.time()
         r = self.client.execute_tasks(tasks)
         print 'stats: %.2f' % (time.time() - t)
@@ -356,21 +355,116 @@ class Tests(unittest.TestCase):
                 ts=time.time())
                 for j in range(100)]
             points = self.client.add_geometries(containers[i].id, pts)
-
-        tasks = [
-            {'operation': snowfloat.task.Operations.map,
-             'resource': 'points',
-             'container_ids': (containers[0].id, containers[1].id),
-             'extras': {'llcrnrlat': -75,
-                        'llcrnrlon': -165,
-                        'urcrnrlat': 75,
-                        'urcrnrlon': 165},
-             'ts_range': (0, time.time())},
-                ]
-
+        
+        tasks = [snowfloat.task.Task(
+                    operation=snowfloat.task.Operations.map,
+                    resource='points',
+                    container_id=(containers[0].id, container[1].id),
+                    extras={'llcrnrlat': -75,
+                            'llcrnrlon': -165,
+                            'urcrnrlat': 75,
+                            'urcrnrlon': 165},
+                    ts_range=(0, time.time()))]
         t = time.time()
         r = self.client.execute_tasks(tasks)
         print 'map: %.2f' % (time.time() - t)
+        self.assertTrue('url' in r[0][0])
+
+    def test_usa(self):
+
+        # add container
+        containers = [snowfloat.container.Container(dat='World')]
+        containers = self.client.add_containers(containers)
+
+        # add cities
+        points = [snowfloat.geometry.Point(coordinates=[-122.41941550000001,
+                                                        37.7749295,
+                                                        0],
+                                           dat='San Francisco',
+                                           ts=1),
+                  snowfloat.geometry.Point(coordinates=[-104.98471790000002,
+                                                        39.737567,
+                                                        0],
+                                           dat='Denver',
+                                           ts=2),
+                  snowfloat.geometry.Point(coordinates=[-71.0597732,
+                                                        42.3584308,
+                                                        0],
+                                           dat='Boston',
+                                           ts=3),
+                  snowfloat.geometry.Point(coordinates=[2.3522219000000177,
+                                                        48.856614,
+                                                        0],
+                                           dat='Paris',
+                                           ts=4),
+                 ]
+        points = self.client.add_geometries(containers[0].id, points)
+
+        # add two polygons: west and east
+        poly_west = snowfloat.geometry.Polygon(
+                        coordinates=[[[-125, 40, 0],
+                                      [-125, 30, 0],
+                                      [-100, 30, 0],
+                                      [-100, 40, 0],
+                                      [-125, 40, 0]]], dat='West')
+        poly_east = snowfloat.geometry.Polygon(
+                        coordinates=[[[-75, 45, 0],
+                                      [-75, 35, 0],
+                                      [-65, 35, 0],
+                                      [-65, 45, 0],
+                                      [-75, 45, 0]]], dat='East')
+        
+        polygons = self.client.add_geometries(containers[0].id,
+            [poly_west, poly_east])
+
+        # look for points contained by the west polygon
+        pts = [e for e in self.client.get_geometries(containers[0].id,
+            type='Point', query='contained', geometry=poly_west)]
+        self.assertEqual(len(pts), 2)
+        self.assertListEqual(pts[0].coordinates,
+            [-122.41941550000001, 37.7749295, 0])
+        self.assertListEqual(pts[1].coordinates,
+            [-104.98471790000002, 39.737567, 0])
+
+        # look for points using distance_lt
+        pl = snowfloat.geometry.Point(coordinates=[2.45, 48.85, 0])
+        pts = [e for e in self.client.get_geometries(containers[0].id,
+            type='Point', query='distance_lt', geometry=pl, distance=20000)]
+        self.assertEqual(len(pts), 1)
+        self.assertListEqual(pts[0].coordinates,
+            [2.3522219000000177, 48.856614, 0])
+
+        # spatial operation distance
+        pl = snowfloat.geometry.Point(coordinates=[2.45, 48.85, 0])
+        pts = [e for e in self.client.get_geometries(containers[0].id,
+            type='Point', spatial_operation='distance', spatial_geometry=pl)]
+        self.assertEqual(len(pts), 4)
+        self.assertListEqual([p.spatial for p in pts],
+            [8958661.49177521, 7866626.32482402, 5538314.71584491,
+             7191.65275350205])
+
+        # task stats
+        tasks = [snowfloat.task.Task(
+                    operation=snowfloat.task.Operations.stats,
+                    resource='points',
+                    container_id=containers[0].id,
+                    ts_range=(0, time.time()))]
+        r = self.client.execute_tasks(tasks)
+        self.assertListEqual(r,
+            [[{u'count': 4, u'distance': 9722488.930599332,
+               u'velocity': 3240829.6435331106}]])
+
+        # task map
+        tasks = [snowfloat.task.Task(
+                    operation=snowfloat.task.Operations.map,
+                    resource='points',
+                    container_id=containers[0].id,
+                    extras={'llcrnrlat': -75,
+                            'llcrnrlon': -165,
+                            'urcrnrlat': 75,
+                            'urcrnrlon': 165},
+                    ts_range=(0, time.time()))]
+        r = self.client.execute_tasks(tasks)
         self.assertTrue('url' in r[0][0])
 
 
