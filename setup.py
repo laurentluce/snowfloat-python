f9b8541f73f54d8ff8b317f371c8c08c2ac81989
Laurent Luce <laurentluce49@yahoo.com>
2013-08-10 21:16:12 -0400

    client 0.6 -> 0.7.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name = "snowfloat",
-    version = "0.6",
+    version = "0.7",
     author = "SnowFloat",
     author_email = "snowfloat@snowfloat.com",
     description = ('Client for the SnowFloat geo API (https://www.snowfloat.com)'),
