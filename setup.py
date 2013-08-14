a1c06318c90dae93a05d3ac8d004d388437d3d01
Laurent Luce <laurentluce49@yahoo.com>
2013-08-14 15:32:53 -0400

    Version 0.8.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name = "snowfloat",
-    version = "0.7",
+    version = "0.8",
     author = "SnowFloat",
     author_email = "snowfloat@snowfloat.com",
     description = ('Client for the SnowFloat geo API (https://www.snowfloat.com)'),
