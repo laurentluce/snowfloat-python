13243b265b1c714f06de7867221cfd861f0d3ce8
Laurent Luce <laurentluce49@yahoo.com>
2013-09-28 12:32:23 -0400

    Grab host setting from config file.

--- snowfloat/settings.py
+++ snowfloat/settings.py
@@ -2,7 +2,6 @@
 import os
 import ConfigParser
 
-HOST = 'api.snowfloat.com:443'
 HTTP_TIMEOUT = 10
 HTTP_RETRIES = 3
 HTTP_RETRY_INTERVAL = 5
@@ -10,13 +9,14 @@ HTTP_RETRY_INTERVAL = 5
 API_KEY_ID = ''
 API_SECRET_KEY = ''
 
-CONFIG = ConfigParser.RawConfigParser()
+CONFIG = ConfigParser.ConfigParser()
 for loc in (os.curdir, os.path.expanduser("~"), "/etc/snowfloat"):
     try:
         with open(os.path.join(loc, "snowfloat.conf")) as source:
             CONFIG.readfp(source)
             API_KEY_ID = CONFIG.get('snowfloat', 'api_key_id')
             API_SECRET_KEY = CONFIG.get('snowfloat', 'api_secret_key')
+            HOST = CONFIG.get('snowfloat', 'host', 'api.snowfloat.com:443')
             break
     except IOError:
         pass
