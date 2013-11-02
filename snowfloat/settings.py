ecb993fa0078028ba532cfeaf965e62421e1c4d7
Laurent Luce <laurentluce49@yahoo.com>
2013-11-02 12:57:59 -0400

    Fix config host default.

--- snowfloat/settings.py
+++ snowfloat/settings.py
@@ -10,14 +10,18 @@ HOST = 'api.snowfloat.com:443'
 API_KEY_ID = ''
 API_SECRET_KEY = ''
 
-CONFIG = ConfigParser.ConfigParser()
+defaults = {
+    'host': 'api.snowfloat.com:443',
+    }
+
+CONFIG = ConfigParser.ConfigParser(defaults)
 for loc in (os.curdir, os.path.expanduser("~"), "/etc/snowfloat"):
     try:
         source = open(os.path.join(loc, "snowfloat.conf"))
         CONFIG.readfp(source)
         API_KEY_ID = CONFIG.get('snowfloat', 'api_key_id')
         API_SECRET_KEY = CONFIG.get('snowfloat', 'api_secret_key')
-        HOST = CONFIG.get('snowfloat', 'host', 'api.snowfloat.com:443')
+        HOST = CONFIG.get('snowfloat', 'host')
         source.close()
         break
     except IOError:
